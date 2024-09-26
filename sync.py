fe57d94f4f802fd0ce1318fc298d22b96a3f9dca
Jayson Dimaranan <joseph_earen@yahoo.com>
2024-09-26 22:32:13 +0800

    Modify sync.py to test search item functionality

--- sync.py
+++ sync.py
@@ -3,8 +3,7 @@ from playwright.sync_api import sync_playwright
 # https://playwright.dev/python/docs/api/class-playwright
 # https://www.youtube.com/watch?v=H2-5ecFwHHQ
 # https://www.youtube.com/watch?v=FK_5SQPq6nY&list=PLYDwWPRvXB8_W56h2C1z5zrlnAlvqpJ6A
-# https://playwright.dev/docs/locators
-# 
+# IMPORTANT REFERENCE FOR LOCATORS https://playwright.dev/docs/locators
 
 # Use a context manager. 
 # with sync_playwright() as playwright > This will close our browser when our code is finished running 
@@ -19,14 +18,18 @@ with sync_playwright() as playwright:
     page.goto('https://www.perthmint.com/')
     # REFERENCE https://stackoverflow.com/questions/75151754/how-can-i-select-an-element-by-id
     # Click on OK button in "Our site uses cookies prompt"
+    # We are locating by CSS selector where element id = onetrust-accept-btn-handler
     page.locator("#onetrust-accept-btn-handler").click()
-    page.wait_for_timeout(5000)
+    page.wait_for_timeout(2000)
     # page.get_by_role("button").and_()
+    # We are locating by CSS selector where classes are as specified below
     page.locator('.header__sub-nav-item.header__nav-item--search').click()
     # Item or product to search is hard code now
-    value_to_search = 'James'
-    page.get_by_placeholder("What are you looking for?").fill(value_to_search)
-    page.wait_for_timeout(5000)
+    value_to_search = 'James Bond Skyfall'
+    # Enter item or product to search in "What are you looking for?" text field
+    
+    page.locator('//*[@id="search-desktop"]/div/input').fill(value_to_search)
+    page.wait_for_timeout(2000)
     
     page.screenshot(path="playwright_sync_demo.png")
     browser.close()
